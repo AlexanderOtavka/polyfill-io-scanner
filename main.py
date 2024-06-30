3d52ccca3f814d9b143bb4766ecb2fd03393657c
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2024-06-30 15:58:17 -0700

    Add context to results and save to csv

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ import io
 import gzip
 import logging
 from tqdm import tqdm
+import datetime
 
 # Define the URL of the gzipped CSV file
 TOP_SITES_URL = "https://raw.githubusercontent.com/zakird/crux-top-lists/main/data/global/current.csv.gz"
 TOP_SITES_CACHE = "./cache-top-sites.csv.gz"
 
 # Maximum number of sites to consider
-MAX_RANK = 1000
-MAX_NUM_SITES = 50
+MAX_RANK = 5000
+MAX_NUM_SITES = MAX_RANK
+# MAX_NUM_SITES = 50
 
 # Keyword to search for in the homepage content
-KEYWORD = "googletagmanager.com"
+KEYWORD = "polyfill.io"
+# KEYWORD = "googletagmanager.com"
 
 # Set up logging
 logging.basicConfig(
@@ -29,6 +32,9 @@ def main():
     logging.info("Starting the script.")
     top_sites = get_top_sites(url=TOP_SITES_URL, cache_file=TOP_SITES_CACHE)
     logging.info(f"Loaded {len(top_sites)} top sites.")
+    
+    ranks = top_sites["rank"].unique()
+    logging.info(f"Unique ranks: {ranks}")
 
     # Filter just the most popular sites
     max_rank_sites = top_sites[top_sites["rank"] <= MAX_RANK]
@@ -41,17 +47,27 @@ def main():
     # Fetch the home page of each site, and scan for polyfill.io
     fetch_homepages_in_parallel(max_rank_sites)
     logging.info("Finished fetching homepages.")
-    
+
     # Filter out sites that have no homepage content
     max_rank_sites = max_rank_sites[max_rank_sites["homepage_content"].str.len() > 0]
     logging.info(f"Filtered to {len(max_rank_sites)} sites with homepage content.")
-    
+
     # Filter sites that use our keyword
-    has_polyfill = max_rank_sites["homepage_content"].str.contains(KEYWORD, case=False, na=False)
-    sites_with_keyword = max_rank_sites[has_polyfill]
+    max_rank_sites["keyword_line"] = max_rank_sites["homepage_content"].apply(
+        find_keyword_context, keyword=KEYWORD
+    )
+    sites_with_keyword = max_rank_sites[max_rank_sites["keyword_line"].notnull()]
     logging.info(f"Found {len(sites_with_keyword)} sites with the keyword '{KEYWORD}'.")
 
-    print(sites_with_keyword[["origin"]])
+    print(sites_with_keyword[["origin", "keyword_line"]])
+
+    # Save the results to a CSV file
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+    filename = f"sites_with_keyword_{timestamp}.csv"
+
+    sites_with_keyword[["origin", "keyword_line"]].to_csv(
+        filename, index=False
+    )
 
 
 def get_top_sites(url, cache_file):
@@ -129,6 +145,19 @@ def fetch_homepage_content(url, timeout=None):
     except Exception as e:
         logging.error(f"Failed to fetch the homepage of {url}. Error: {e}")
         return ""
+
+
+def find_keyword_context(text: str, keyword: str):
+    for i, line in enumerate(text.splitlines()):
+        line = line.lower().strip()
+        keyword_index = line.find(keyword)
+        if keyword_index == -1:
+            continue
+
+        context = line[max(0, keyword_index - 50) : keyword_index + 50]
+        return f"{i}: {context}"
+
+    return None
 
 
 if __name__ == "__main__":
